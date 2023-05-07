4f36bbddf290c489e9b4ce464d72ccb56fba9142
Salma Sali <salianovaj@gmail.com>
2023-05-07 08:21:43 -0400

    add: more tests

--- cypress/e2e/testStructure.cy.js
+++ cypress/e2e/testStructure.cy.js
@@ -17,8 +17,17 @@ afterEach('like @AfterMethod in TestNG', () => {
 })
 it('Opening a Web Application', () => {
     cy.visit('/registration_form');
-    
- 
 })
-
+it('Test 2', () => {
+    expect(false).to.equal(false);
+})
+it('Test 3', () => {
+    expect(false).not.to.equal(true);
+})
+it('Test 4', () => {
+    expect(5).to.equal(5);
+})
+it('Test 5', () => {
+    expect(true).to.equal('5'==5);
+})
 })
